5c69e98540edd2182d776ff6f1c2106bc914dc8e
Ariel Arnedo <arieljosearnedocervantes@gmail.com>
2019-01-10 15:58:11 -0500

    agregando el pipeline jenkinsfile

--- src/main/java/co/com/ceiba/parkingchallenge/models/Parking.java
+++ src/main/java/co/com/ceiba/parkingchallenge/models/Parking.java
@@ -1,5 +1,23 @@
 package co.com.ceiba.parkingchallenge.models;
 
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Parking {
 	
+	private List<Vehicle> vehicles;
+	
+	public Parking() {
+		this.vehicles = new ArrayList<>();
+	}
+	
+	public Parking(List<Vehicle> vehicles) {
+		this.vehicles = vehicles;
+	}
+	
+	public void addVehicle(Vehicle vehicle) {
+		this.vehicles.add(vehicle);
+	}
 }
